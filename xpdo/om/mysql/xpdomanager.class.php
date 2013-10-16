efcf251b1424663f0a9c02e52d58a22fc4a46a45
Petri Rautiainen <the_dunnock@outlook.com>
2013-10-16 21:50:02 +0300

    Fixes MySQL 5.6 default values on timestamp field types when ON UPDATE CURRENT_TIMESTAMP is set

--- xpdo/om/mysql/xpdomanager.class.php
+++ xpdo/om/mysql/xpdomanager.class.php
@@ -436,7 +436,10 @@ class xPDOManager_mysql extends xPDOManager {
             $extra= ' ' . $meta['extra'];
         }
         $default= '';
-        if (isset ($meta['default']) && !preg_match($lobsPattern, $dbtype)) {
+        if (in_array($this->xpdo->driver->getPhpType($dbtype), $datetimeStrings) && preg_match('/ON UPDATE CURRENT_TIMESTAMP/',$attributes) == 1 &&
+            (!isset($meta['default']) || $meta['default'] == null || strtoupper($meta['default']) == 'NULL' || $meta['default'] == '0000-00-00 00:00:00')) {
+            $default = ' DEFAULT CURRENT_TIMESTAMP';
+        } else if (isset ($meta['default']) && !preg_match($lobsPattern, $dbtype)) {
             $defaultVal= $meta['default'];
             if (($defaultVal === null || strtoupper($defaultVal) === 'NULL') || (in_array($this->xpdo->driver->getPhpType($dbtype), $datetimeStrings) && $defaultVal === 'CURRENT_TIMESTAMP')) {
                 $default= ' DEFAULT ' . $defaultVal;
