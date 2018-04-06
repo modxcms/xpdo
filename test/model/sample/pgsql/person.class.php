bbb48d4ae3d9fc361fb5fbc29b307c5f3e714cbb
Ivan Klimchuk <ivan.klimchuk@1pt.com>
2018-04-06 23:53:52 +0300

    Fixed pgsql samples and schema for legacy tests

--- test/model/sample/pgsql/person.class.php
+++ test/model/sample/pgsql/person.class.php
@@ -1,3 +1,11 @@
 <?php
+/**
+ * Include the required database-independent parent class.
+ */
 require_once (dirname(dirname(__FILE__)) . '/person.class.php');
+/**
+ * Represents a Person.
+ * @see person.map.inc.php
+ * @package sample.pgsql
+ */
 class Person_pgsql extends Person {}
