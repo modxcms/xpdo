567b4b7cefa59d0e64c75362b2014ff6152ab4c0
Jason Coward <jason@opengeek.com>
2022-01-06 10:38:51 -0700

    Fix deprecated warnings for 8.1
    For real this time!

--- src/xPDO/xPDOMap.php
+++ src/xPDO/xPDOMap.php
@@ -38,6 +38,7 @@ class xPDOMap implements ArrayAccess
         return isset($this->map[$offset]);
     }
 
+    #[\ReturnTypeWillChange]
     public function offsetGet($offset)
     {
         if (!isset($this->map[$offset])) {
@@ -46,12 +47,12 @@ class xPDOMap implements ArrayAccess
         return $this->map[$offset];
     }
 
-    public function offsetSet($offset, $value)
+    public function offsetSet($offset, $value): void
     {
         $this->map[$offset] = $value;
     }
 
-    public function offsetUnset($offset)
+    public function offsetUnset($offset): void
     {
         unset($this->map[$offset]);
     }
